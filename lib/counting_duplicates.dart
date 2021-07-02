7784660961745260642d180a1a631d7b72cb90f6
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-02 12:29:41 -0300

    dumb code 	em lib/: código funcionando, porém de um jeito feio e burro 	em test/: exemplos de teste com heitor

--- lib/counting_duplicates.dart
+++ lib/counting_duplicates.dart
@@ -1,4 +1,21 @@
-int duplicateCount(var text){
-  // your code goes below
-  return -1;
+int duplicateCount(String? text)
+{
+  var count = 0;
+  var repeat_letters = <String>[];;
+
+  for(var index = 0; index < text!.length - 1; index++)
+  {
+    for(var load = index + 1; load < text.length; load++)
+    {
+      if((text[index] == text[load] || text[index] == text[load].toUpperCase() || text[index] == text[load].toLowerCase()) && (!repeat_letters.contains(text[index]) && !repeat_letters.contains(text[index].toLowerCase()) && !repeat_letters.contains(text[index].toUpperCase())))
+      {
+        count++;
+        repeat_letters.add(text[index]);
+        print(repeat_letters);
+        break;
+      }
+    }
+  }
+
+  return count;
 }
--- test/counting_duplicates_test.dart
+++ test/counting_duplicates_test.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
 
 import 'package:counting_duplicates/counting_duplicates.dart';
-/*import 'package:test/test.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(duplicateCount(), 42);
+  test('teste com heitor', () {
+    expect(duplicateCount('heitor'), 0);
   });
-}*/
+}
