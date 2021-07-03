8c146b1a58f1f93a7a763836228de03c694d0890
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-03 12:01:32 -0300

    input test added

--- test/counting_duplicates_test.dart
+++ test/counting_duplicates_test.dart
@@ -1,10 +1,15 @@
-import 'dart:developer';
-
 import 'package:counting_duplicates/counting_duplicates.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('teste com heitor', () {
-    expect(duplicateCount('heitor'), 0);
+  test('input test', () {
+    expect(duplicateCount(''), equals(0));
+    expect(duplicateCount('oioi'), equals(2));
+    expect(duplicateCount('thais'), equals(0));
+    expect(duplicateCount('aaaAAA'), equals(1));
+    expect(duplicateCount('aabbcde'), equals(2));
+    expect(duplicateCount('blablaBlabla'), equals(3));
+    expect(duplicateCount('Indivisibility'), equals(1));
+    expect(duplicateCount('computer science'), equals(2));
   });
 }
