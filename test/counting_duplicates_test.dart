d16f60b6f2c898676e41a02841bac9ceededa1db
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-03 12:18:45 -0300

    tests on group
    Os testes foram separados por grupo, assim é possível executar todos de uma vez, como cada um por vez

--- test/counting_duplicates_test.dart
+++ test/counting_duplicates_test.dart
@@ -2,14 +2,21 @@ import 'package:counting_duplicates/counting_duplicates.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('input test', () {
-    expect(duplicateCount(''), equals(0));
-    expect(duplicateCount('oioi'), equals(2));
-    expect(duplicateCount('thais'), equals(0));
-    expect(duplicateCount('aaaAAA'), equals(1));
-    expect(duplicateCount('aabbcde'), equals(2));
-    expect(duplicateCount('blablaBlabla'), equals(3));
-    expect(duplicateCount('Indivisibility'), equals(1));
-    expect(duplicateCount('computer science'), equals(2));
+  group('input test | ', () {
+    test("'' => 0", () {
+      expect(duplicateCount(''), equals(0));
+    });
+
+    test('oioi => 2', () {
+      expect(duplicateCount('oioi'), equals(2));
+    });
+
+    test('Indivisibility => 1', () {
+      expect(duplicateCount('Indivisibility'), equals(1));
+    });
+
+    test('computer science => 2', () {
+      expect(duplicateCount('computer science'), equals(2));
+    });
   });
 }
